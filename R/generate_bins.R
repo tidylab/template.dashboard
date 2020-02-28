bcbfe8b1018d0e39b9cf6129d052353d4afc42fd
Harel Lustiger <harel.l@medialab.local>
2020-02-28 15:14:39 +1300

    added documentation

--- R/generate_bins.R
+++ R/generate_bins.R
@@ -3,6 +3,10 @@
 #' @description This is a demonstration of how to use functions in Shiny that
 #'   are installed in an adjustment package
 #'
+#' @param x (`numeric`) A numeric variable.
+#' @param n_bins (`integer`) A positive integer specifying the number of bins in
+#'   the histogram.
+#'
 #' @return Bins boundaries
 #' @export
 #'
